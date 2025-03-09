133a0f430835e738ac6b8fbafa9fcaa20373a7a4
RtoonStrer <155787067+crazzikk@users.noreply.github.com>
2025-03-09 16:54:42 +0300

    Create we.lua

---
+++ we.lua
@@ -0,0 +1,5 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+
+local Window = Library.CreateLib("DioRUS", "AWS")
+
+local Tab = Window:NewTab("Egg")
